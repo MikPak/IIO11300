cdca50bab83fa98cc9890db117ca79d27d90aa8d
Mikko Pakkanen <h8699@student.jamk.fi>
2016-02-09 17:29:50 +0200

    OudotOliot 1.0

---
+++ IIO11300Vktehtavat/OudotOliot/MainWindow.xaml.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OudotOliot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pelaaja> pelaajatList = new List<Pelaaja>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.seuraComboBox.Items.Add("Blues");
+            this.seuraComboBox.Items.Add("HIFK");
+            this.seuraComboBox.Items.Add("HPK");
+            this.seuraComboBox.Items.Add("Ilves");
+            this.seuraComboBox.Items.Add("JYP");
+            this.seuraComboBox.Items.Add("KalPa");
+            this.seuraComboBox.Items.Add("KooKoo");
+            this.seuraComboBox.Items.Add("Kärpät");
+            this.seuraComboBox.Items.Add("Lukko");
+            this.seuraComboBox.Items.Add("Pelicans");
+            this.seuraComboBox.Items.Add("SaiPa");
+            this.seuraComboBox.Items.Add("Sport");
+            this.seuraComboBox.Items.Add("Tappara");
+            this.seuraComboBox.Items.Add("TPS");
+            this.seuraComboBox.Items.Add("Ässät");
+        }
+
+        private void luoPelaajaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(etunimiTxtBox.Text != "" && sukunimiTxtBox.Text != "" && siirtohintaTxtBox.Text != ""
+                && seuraComboBox.SelectedItem != null)
+            {
+                try
+                {
+                    Pelaaja pelaaja = new Pelaaja(etunimiTxtBox.Text, sukunimiTxtBox.Text, 
+                       seuraComboBox.SelectedItem.ToString(), siirtohintaTxtBox.Text);
+
+                    //Console.WriteLine(pelaajatList.Count);
+                    if (!pelaajatList.Any(x => x.Etunimi == pelaaja.Etunimi && x.Sukunimi == pelaaja.Sukunimi))
+                    {
+                        pelaajatListBox.Items.Add(pelaaja);
+                        pelaajatList.Add(pelaaja);
+
+                        //statusBar1.Text = "asd";
+                        /*
+                        System.Windows.pelaajatListBox.DisplayMember = "UserName";
+                        pelaajatListBox.ValueMember = "UserId";
+
+                        pelaajatListBox.Items.Add(new YourItem
+                        {
+                            UserName = "FooName",
+                            UserId = "FooId"
+                        });
+                         */
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private void talletaPelaajaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pelaajatListBox.SelectedItem != null)
+            {
+                Pelaaja pelaaja = (Pelaaja)pelaajatListBox.SelectedItem;
+                pelaaja.Etunimi = etunimiTxtBox.Text;
+                pelaaja.Sukunimi = sukunimiTxtBox.Text;
+                pelaaja.Siirtohinta = siirtohintaTxtBox.Text;
+                pelaaja.Seura = seuraComboBox.SelectedValue.ToString();
+            }
+        }
+
+        private void poistaPelaajaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pelaajatListBox.SelectedItem != null)
+            {
+                Pelaaja pelaaja = (Pelaaja)pelaajatListBox.SelectedItem;
+
+                // Remove from List
+                int index = pelaajatList.FindIndex(a => a.Etunimi == pelaaja.Etunimi && a.Sukunimi == pelaaja.Sukunimi);
+                pelaajatList.RemoveAt(index);
+                Console.WriteLine(pelaajatList.Count);
+
+                // Reset pointer
+                pelaaja = null;
+                // Remove item from ListBox
+                pelaajatListBox.Items.Remove(pelaajatListBox.SelectedItem);
+
+                // Reset text inputs
+                etunimiTxtBox.Text = "";
+                sukunimiTxtBox.Text = "";
+                siirtohintaTxtBox.Text = "";
+                seuraComboBox.SelectedIndex = -1;
+            }
+        }
+
+        private void kirjoitaPelaajatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (pelaajatListBox.Items.Count != 0)
+            {
+                Microsoft.Win32.SaveFileDialog savefile = new Microsoft.Win32.SaveFileDialog();
+                savefile.FileName = "Pelaajat";
+                savefile.Filter = "Text files (*.dat)|*.dat|All files (*.*)|*.*";
+
+                Nullable<bool> result = savefile.ShowDialog();
+                if (result == true)
+                {
+                    //serialisoidaan XML:ksi
+                    OudotOliot.Serialisointi.SerialisoiXml(@"D:\testi.xml", pelaajatList);
+                }
+            }
+        }
+
+        private void lopetusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void seuraComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void pelaajatListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void pelaajatListBox_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (pelaajatListBox.SelectedItem != null)
+            {
+                Pelaaja pelaaja = (Pelaaja)pelaajatListBox.SelectedItem;
+                etunimiTxtBox.Text = pelaaja.Etunimi;
+                sukunimiTxtBox.Text = pelaaja.Sukunimi;
+                siirtohintaTxtBox.Text = pelaaja.Siirtohinta;
+                int index = seuraComboBox.Items.IndexOf(pelaaja.Seura);
+                //Console.WriteLine(index);
+                seuraComboBox.SelectedIndex = index;
+            }
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/OudotOliot/Pelaaja.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OudotOliot
+{
+    public class Pelaaja
+    {
+        private string etunimi, sukunimi, kokonimi, esitysnimi, seura, siirtohinta;
+
+        public string Etunimi
+        {
+            get { return etunimi; }
+            set { etunimi = value; }
+        }
+
+        public string Sukunimi
+        {
+            get { return sukunimi; }
+            set { sukunimi = value; }
+        }
+
+        public string Kokonimi
+        {
+            get { return kokonimi; }
+        }
+        public string Esitysnimi
+        {
+            get { return esitysnimi; }
+        }
+        public string Siirtohinta
+        {
+            get { return siirtohinta; }
+            set { siirtohinta = value; }
+        }
+
+        public string Seura
+        {
+            get { return seura; }
+            set { seura = value; }
+        }
+
+        public Pelaaja()
+        {
+
+        }
+        public Pelaaja(string etunimi, string sukunimi, string seura, string siirtohinta)
+        {
+            this.etunimi = etunimi;
+            this.sukunimi = sukunimi;
+            this.kokonimi = etunimi + " " + sukunimi;
+            this.esitysnimi = etunimi + " " + sukunimi + ", " + seura;
+            this.seura = seura;
+            this.siirtohinta = siirtohinta;
+        }
+
+        public static void SaveDataToFile(List<Pelaaja> dataa, string filu)
+        {
+            // kirjoitetaan data tiedostoon, jos tiedosto on jo olemassa liitetään se olemassa olevaan
+            try
+            {
+                //tutkitaan onko tiedosto olemassa
+                if (!System.IO.File.Exists(filu))
+                {
+                    // luodaan uusi
+                    using (StreamWriter sw = File.CreateText(filu))
+                    {
+                        // Käydään kokoelma läpi ja kirjoitetaan kukin mittausdata omalle riville
+                        foreach (var item in dataa)
+                        {
+                            sw.WriteLine(item);
+                        }
+                    }
+                }
+                else
+                {
+                    // Lisätään olemassa olevaan tiedostoon
+                    using (StreamWriter sw = File.AppendText(filu))
+                    {
+                        foreach (var item in dataa)
+                        {
+                            sw.WriteLine(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
