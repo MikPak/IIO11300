3a078120fbbd6d7490888cbe40e14e1a351f8d98
Mikko Pakkanen <mikkopak@gmail.com>
2016-02-17 11:11:20 +0200

    H5MoviesXML koulu final

--- IIO11300Vktehtavat/H5MoviesXML/Movies2.xaml.cs
+++ IIO11300Vktehtavat/H5MoviesXML/Movies2.xaml.cs
@@ -26,7 +26,27 @@ namespace H5MoviesXML
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                string filu = xdpMovies.Source.LocalPath;
+                xdpMovies.Document.Save(filu);
+                MessageBox.Show("Tallennettu");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+
+        private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            // Lisätään XMLDcoumenttiin uusi elementti
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            // Poistetaan XmlDocumentista valittu elementti
+        }
+
     }
 }
